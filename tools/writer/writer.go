399fcfd449c12638d177d8b060c5b3093c52bba0
Rifky Manuel Satyana <rifky.satyana@starcapital.co.id>
2023-12-19 20:21:15 +0700

    fix: writer new methods

--- tools/writer/writer.go
+++ tools/writer/writer.go
@@ -18,18 +18,32 @@ func NewWriter() *Writer {
 	}
 }
 
-func (w *Writer) Error(format string, args ...any) {
+func (w *Writer) Errorf(format string, args ...any) {
 	date := new(strings.Builder)
 	log.New(date, "", log.LstdFlags).Print()
 
 	w.color.Add(color.FgRed).Printf("%s %s", strings.TrimSpace(date.String()), fmt.Sprintf(format, args...))
 }
 
-func (w *Writer) Log(format string, args ...any) {
+func (w *Writer) Error(message string, args ...any) {
+	date := new(strings.Builder)
+	log.New(date, "", log.LstdFlags).Print()
+
+	w.color.Add(color.FgRed).Printf("%s %s", strings.TrimSpace(date.String()), fmt.Sprintln(append([]any{message}, args...)...))
+}
+
+func (w *Writer) Logf(format string, args ...any) {
 	date := new(strings.Builder)
 	log.New(date, "", log.LstdFlags).Print()
 
 	w.color.Add(color.FgGreen).Printf("%s %s", strings.TrimSpace(date.String()), fmt.Sprintf(format, args...))
+}
+
+func (w *Writer) Log(message string, args ...any) {
+	date := new(strings.Builder)
+	log.New(date, "", log.LstdFlags).Print()
+
+	w.color.Add(color.FgGreen).Printf("%s %s", strings.TrimSpace(date.String()), fmt.Sprintln(append([]any{message}, args...)...))
 }
 
 func (w *Writer) Warn(format string, args ...any) {
